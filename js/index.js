f1daaa2f5d98f49720e52f4e2e620f73c29c80b2
Tania Sanchez <83056378+Tan-uxr@users.noreply.github.com>
2021-10-04 03:05:00 +0000

    modal, hover buttons

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 //console.log("Your index.js file is loaded correctly!");
 
+var modal = document.getElementById("myModal");
+var mainPhoto = document.getElementById("mainPhoto");
+var span = document.getElementsByClassName("modal-close-button")[0];
 
-$("").on("click", function() {
-  
-});
+mainPhoto.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// cerrar el modal cuando se da click fuera de él
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
